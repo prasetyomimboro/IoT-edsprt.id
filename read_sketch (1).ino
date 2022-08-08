f21b94a0797353b8e670e7ddaad3fe532a3be2d9
Prasetyo Mimboro <84126274+prasetyomimboro@users.noreply.github.com>
2022-08-08 20:06:31 +0700

    Add files via upload

---
+++ read_sketch (1).ino
@@ -0,0 +1,53 @@
+const int analogApi = A0;
+const int analogVoice = A1;
+const int signalLed = 4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(signalLed, OUTPUT);
+
+  Serial.print("Memulai program ");
+
+  for (int i = 0; i < 3; i++) {
+    Serial.print(".");
+    delay(1000);
+  }
+}
+
+void loop() {
+  readApi();
+  delay(100);
+  readVoice();
+  delay(100);
+}
+
+void readApi() {
+  int val = analogRead(analogApi);
+
+  Serial.println();
+  Serial.print("Nilai Sensor Api: ");
+  Serial.print(val);
+
+  if (val < 200) {
+    Serial.println("\nLed Aktif");
+    digitalWrite(signalLed, 1);
+    delay(1000);
+    digitalWrite(signalLed, 0);
+    Serial.println("Led tidak Aktif");
+  }
+}
+
+void readVoice() {
+  int val = analogRead(analogVoice);
+  Serial.println();
+  Serial.print("Nilai Sensor Suara: ");
+  Serial.print(val, DEC);
+
+  if (val > 150) {
+    Serial.println("\nLed Aktif");
+    digitalWrite(signalLed, 1);
+    delay(1000);
+    digitalWrite(signalLed, 0);
+    Serial.println("Led tidak Aktif");
+  }
+}
